6bb085f449696394fbd08d6e72df946891dad59d
gbider <gabes.bider20@bcp.org>
2018-05-09 12:17:13 -0700

    You lose function works, auto scrolls, increases speed

--- playerscroll.py
+++ playerscroll.py
@@ -8,6 +8,14 @@ import math, random, sys, os, time
 import pygame
 from pygame.locals import *
 from collision import *
+from loss import *
+#variables
+time = 0
+factor = 0
+speed = 0
+score = 0
+prevScore = 1
+gameRound = 0
 
 
 # exit the program
@@ -64,34 +72,39 @@ playerVelocityX = 0
 playerVelocityY = 0
 ###################
 
-         
+pass_limit = 5
 
 #COLLISION
 def intersect():
     if (playerPosY < 550 and  playerPosY >= rect1PosX - 50) and (playerPosY < 550 and playerPosY <= rect1PosX + 50):      
-        print("The Circle's height is " + str(playerPosY) + " and the rectangle is at " + str(rect1PosX))
-        return True 
+        #print("The Circle's height is " + str(playerPosY) + " and the rectangle is at " + str(rect1PosX))
+        return True
+    else:
+        return False
 
 # main loop
-time = 0
-factor = 0
-while True:
+playing = True
+
+while playing:
 	#check if close game
 	events()
 	#short hand to call key presses
 	k = pygame.key.get_pressed()
 	
-	if k[K_RIGHT]:
-		playerVelocityX = 2.5
-	elif k[K_LEFT]:
-		playerVelocityX = -2.5
-	elif k[K_SPACE]:
+	# if k[K_RIGHT]:
+	# 	playerVelocityX = 2.5
+	# elif k[K_LEFT]:
+	# 	playerVelocityX = -2.5
+	# else:
+	# 	playerVelocityX = 0
+
+	if k[K_SPACE]:
 		if playerPosY <= 610:
 			playerVelocityY = -2.5
 	elif k[K_d]:
 		print(str(playerPosX) + ", " + str(playerPosY))
-	else:
-		playerVelocityX = 0
+
+	playerVelocityX = speed
 
 	#max cords to limit and control jumping
 	if playerPosY <= 500:
@@ -100,7 +113,7 @@ while True:
 		playerVelocityY = 0
 		playerPosY = 610
 	
-	#moving player on x axis
+	#moving player on x axis		
 	playerPosX += playerVelocityX
 
 	circlePosX = startScrollingPosX
@@ -120,13 +133,13 @@ while True:
 	pygame.draw.circle(DS, WHITE, (int(circlePosX), int(playerPosY - 25)), int(circleRadius), 0)
 	
 	#Rectangle coord assigning
-	factor += 1
+	factor += (speed + 0.5)
 	rect1PosX = 1280 - factor
 	rect1PosY = 410
 
-	#check if redraw needed
-	if rect1PosX < -50:
-		factor = 0
+
+
+	
 	#draw rectangle
 	#pygame.draw.rect(screen, color, (x,y,width,height), thickness)
 	pygame.draw.rect(DS, BROWN, (rect1PosX, rect1PosY, 100, 50), 0)
@@ -135,6 +148,31 @@ while True:
 	intersect()
 	if intersect() == True:
 		print("YOU SCORED A POINT")
+		score += 1
+		prevScore = score
+		print("Your score is " + str(score))
+	#elif intersect() == False:
+	
+	#if rect1PosX > 500:
+		#shouldI = 1
+	#if rect1PosX < 450 and shouldI == 0:
+		#print("Loss")
+
+	if rect1PosX < 450 and prevScore != score:
+		playing = False
+		message_display("YOU LOSE")
+
+	#check if redraw needed
+	if rect1PosX < -50:
+		factor = 0
+		gameRound += 1
+		prevScore = (score + 5)
+	
+
+
+
+	speed += 0.001
+	
 
 	#load character
 	#DS.blit(char,(25,600))
@@ -143,5 +181,5 @@ while True:
 	pygame.display.update()
 	CLOCK.tick(FPS)
 	DS.fill(BLACK)
-############
 
+############
